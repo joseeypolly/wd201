5705ee696cc67982a8ac6694a5ec992bac154b7d
Amujuri Joseph Paul <joseypaul312gmail.com>
2023-12-27 18:07:02 +0530

    updated app.js

--- todo-manager/index.js
+++ todo-manager/index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log("Started express server at port 3000");
 });
--- todo-manager/models/todo.js
+++ todo-manager/models/todo.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -8,17 +7,47 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // static associate(models) {
-    //   // define association here
-    // }
+    static associate(models) {
+      // define association here
+    }
 
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
-    static getTodos() {
+    static getTodos () {
       return this.findAll();
     }
+
+    static getOverdues(){
+      return this.findAll({
+        where:{
+          dueDate: {
+            [Op.lt]: new Date().toISOString().split("T")[0]
+          }
+        }
+      });
+    }
+
+    static getDuetoday(){
+      return this.findAll({
+        where: {
+          dueDate: new Date().toISOString().split("T")[0]
+        }
+      });
+    }
+
+    static getDueLater(){
+      let tom = new Date().setDate(new Date().getDate()+1);
+      return this.findAll({
+        where:{
+          dueDate: {
+            [Op.gt]: tom 
+          }
+        }
+      });
+    }
+
 
     markAsCompleted() {
       return this.update({ completed: true });
