a5cb579e1f9a755798d7f31be0051d10ba5d4424
Amujuri Joseph Paul <joseypaul312gmail.com>
2023-12-28 11:57:06 +0530

    updated todo-manager

--- todo-manager/index.js
+++ todo-manager/index.js
@@ -1,6 +1,5 @@
 const app = require("./app");
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Started express server at port 3000");
