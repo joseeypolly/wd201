84d268fdd1a18c9bb7fd2b0152e2bcce803ec20a
Amujuri Joseph Paul <136996704+joseeypolly@users.noreply.github.com>
2023-12-29 00:06:28 +0530

    Update index.js l9

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,6 +1,6 @@
+/* eslint-disable no-undef */
 const app = require("./app");
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log("Started express server at port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server listening at port - ${port}`);
 });
