bd9f0871f3c2f8e2f5cc921ff3bd860013d26136
Amujuri Joseph Paul <joseypaul312gmail.com>
2023-12-28 18:31:15 +0530

    Updated tiny-csrf

--- todo-manager/app.js
+++ todo-manager/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-var csrf = require("csurf");
+var csrf = require("tiny-csrf");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
@@ -8,7 +8,7 @@ const path = require("path");
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("Shh!! some secret string"));
-app.use(csrf({ cookie : true}));
+app.use(csrf("this_could_be_32_characters_long", ["POST","PUT", "DELETE"]));
 
 //Set view Engine as EJS
 app.set("view engine", "ejs");
